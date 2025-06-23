f9eda36529319b49c7b6313bf1c33331bb8b7e4a
Hui-nuna <dofl880@gmail.com>
2025-06-23 10:51:27 +0900

    Commit_4 파일 수정함.

--- Git_ignore_test/src/main/java/nocommit_some/Commit_4.java
+++ Git_ignore_test/src/main/java/nocommit_some/Commit_4.java
@@ -8,6 +8,7 @@ public class Commit_4 {
 		Commit_4.java 파일은 ignore 대상이 아닌 파일이므로 Commit 대상이 됩니다. 
 	*/
 		System.out.println("## Commit_4.java 파일입니다. ##");		
+		System.out.println("## Commit_4.java 수정합니다. ##");		
 
 	}
 
